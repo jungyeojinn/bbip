58615f8e8af9a937bc89200a854dde4162e5bffd
97choijw <97choijw@gmail.com>
2024-09-11 17:16:22 +0900

    fix: 프로젝트 트리 수정

--- be/src/main/java/com/bbip/BeApplication.java
+++ be/src/main/java/com/bbip/BeApplication.java
@@ -1,4 +1,4 @@
-package org.example.be;
+package com.bbip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BeApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BeApplication.class, args);
+	}
 
 }
--- be/src/test/java/com/bbip/BeApplicationTests.java
+++ be/src/test/java/com/bbip/BeApplicationTests.java
@@ -1,4 +1,4 @@
-package org.example.be;
+package com.bbip;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BeApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
