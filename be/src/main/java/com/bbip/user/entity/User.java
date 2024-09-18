e8f894222fb4fd86afe8022973544b2b1405badf
97choijw <97choijw@gmail.com>
2024-09-18 15:16:38 +0900

    feat: user Entity 생성

---
+++ be/src/main/java/com/bbip/user/entity/User.java
@@ -0,0 +1,32 @@
+package com.bbip.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false, length = 50)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String nickname;
+
+    @Column(nullable = false)
+    private Boolean deleted;
+
+}
