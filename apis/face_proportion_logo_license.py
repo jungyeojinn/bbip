004a255987d607afbe5c79890603af54ae96eaa6
정여진 <yeojin9905@naver.com>
2024-10-10 18:08:46 +0900

    feat: weapon 추가

--- apis/face_proportion_logo_license.py
+++ apis/face_proportion_logo_license.py
@@ -80,158 +80,165 @@ class MediaTransformTrack(MediaStreamTrack):
         global frame_count, fps_start_time  # 글로벌 변수 사용
         global model, known_face_embeddings, known_face_names, trackers, tracker_faces
 
+        if self.kind == 'video':
 
-        start_time = time.perf_counter()  # 시작 시간 기록
+            start_time = time.perf_counter()  # 시작 시간 기록
 
-        if self.is_processing:
-            print("현재 프레임 처리 중, 이번 프레임 패스")
-            return await self.track.recv()
+            if self.is_processing:
+                print("현재 프레임 처리 중, 이번 프레임 패스")
+                return await self.track.recv()
 
-        self.is_processing = True
-        print("비디오 변환 처리")
+            self.is_processing = True
+            print("비디오 변환 처리")
 
-        frame_start_time = time.perf_counter()
-        frame = await self.track.recv()
-        frame_end_time = time.perf_counter()
-        print(f"프레임 수신 시간: {frame_end_time - frame_start_time:.4f} 초")
+            frame_start_time = time.perf_counter()
+            frame = await self.track.recv()
+            frame_end_time = time.perf_counter()
+            print(f"프레임 수신 시간: {frame_end_time - frame_start_time:.4f} 초")
 
-        #img_conversion_start_time = time.perf_counter()
-        #img = yolo.process_frame(img, model)
-        #img = frame.to_ndarray(format="bgr24")
-        #img_conversion_end_time = time.perf_counter()
-        #print(f"프레임 변환 시간: {img_conversion_end_time - img_conversion_start_time:.4f} 초")
+            #img_conversion_start_time = time.perf_counter()
+            #img = yolo.process_frame(img, model)
+            #img = frame.to_ndarray(format="bgr24")
+            #img_conversion_end_time = time.perf_counter()
+            #print(f"프레임 변환 시간: {img_conversion_end_time - img_conversion_start_time:.4f} 초")
 
-        processing_start_time = time.perf_counter()
-        # VideoFrame을 YUV420P 형식의 NumPy 배열로 변환
-        image = frame.to_ndarray(format="yuv420p")
-        # YUV420P에서 BGR로 변환
-        image_bgr = cv2.cvtColor(image, cv2.COLOR_YUV2BGR_I420)
-        
-        
-        if frame_count % 20 == 0 or len(trackers) == 0:
-        
-            # 모델 예측
-            detection = model(image_bgr)[0]
-            object_boxes = []     # 한 프레임 내에서 검출된 객체들
-            trackers = []       # 추적기
+            processing_start_time = time.perf_counter()
+            # VideoFrame을 YUV420P 형식의 NumPy 배열로 변환
+            image = frame.to_ndarray(format="yuv420p")
+            # YUV420P에서 BGR로 변환
+            image_bgr = cv2.cvtColor(image, cv2.COLOR_YUV2BGR_I420)
+            
+            
+            if frame_count % 20 == 0 or len(trackers) == 0:
+            
+                # 모델 예측
+                detection = model(image_bgr)[0]
+                object_boxes = []     # 한 프레임 내에서 검출된 객체들
+                trackers = []       # 추적기
 
-            for data in detection.boxes.data.tolist():
-                confidence = float(data[4])
-                if confidence < 0.6:
-                    continue
+                for data in detection.boxes.data.tolist():
+                    confidence = float(data[4])
+                    if confidence < 0.6:
+                        continue
 
-                xmin, ymin, xmax, ymax = int(data[0]), int(data[1]), int(data[2]), int(data[3])
-                w = xmax - xmin
-                h = ymax - ymin
-                object_boxes.append((xmin, ymin, w, h))
+                    xmin, ymin, xmax, ymax = int(data[0]), int(data[1]), int(data[2]), int(data[3])
+                    w = xmax - xmin
+                    h = ymax - ymin
+                    object_boxes.append((xmin, ymin, w, h))
 
-            # YOLO로 감지된 얼굴에 대해 CSRT 추적기 추가
-            for (xmin, ymin, w, h) in object_boxes:
-                # 추적된 객체 부분을 잘라냄
-                object_region = image_bgr[ymin:ymin+h, xmin:xmin+w]
-                if object_region.size > 0:
-                    tracker = cv2.TrackerCSRT_create()
-                    tracker.init(image_bgr, (xmin, ymin, w, h))
-                    name = "face" #얼굴이면 추적기에 face로 등록해놓음
-                    trackers.append((tracker, (xmin,ymin,w,h),object_region.size, name))
-        
-            detection = model2(image_bgr)[0]    #로고 및 차번호판 감지
-
-            for data in detection.boxes.data.tolist():
-                confidence = float(data[4])
-                if confidence < 0.6:
-                    continue
-
-                xmin, ymin, xmax, ymax, label = int(data[0]), int(data[1]), int(data[2]), int(data[3]), int(data[4])
-                w = xmax - xmin
-                h = ymax - ymin
-                object_boxes.append((xmin, ymin, w, h))
-
-                # YOLO로 감지된 객체에 대해 CSRT 추적기 추가
+                # YOLO로 감지된 얼굴에 대해 CSRT 추적기 추가
                 for (xmin, ymin, w, h) in object_boxes:
                     # 추적된 객체 부분을 잘라냄
                     object_region = image_bgr[ymin:ymin+h, xmin:xmin+w]
                     if object_region.size > 0:
                         tracker = cv2.TrackerCSRT_create()
                         tracker.init(image_bgr, (xmin, ymin, w, h))
-                        name="logo"
-                        if(label==0): name="license"                    
-                        trackers.append((tracker, (xmin,ymin,w,h),object_region.size,name))
+                        name = "face" #얼굴이면 추적기에 face로 등록해놓음
+                        trackers.append((tracker, (xmin,ymin,w,h),object_region.size, name))
             
-        # 넓이에 따라 내림차순 정렬
-        trackers.sort(key=lambda x: x[2], reverse=True)  # object region를 기준으로 정렬
-        
-        target_selected = False
+                detection = model2(image_bgr)[0]    #로고 및 차번호판 감지
 
-        # 추적된 객체들 업데이트
-        for i, (tracker, bbox, object_region, label) in enumerate(trackers): 
-            ret, bbox = tracker.update(image_bgr)
-            if ret:
-                (xmin, ymin, w, h) = [int(v) for v in bbox]
-                xmax = xmin + w
-                ymax = ymin + h
+                for data in detection.boxes.data.tolist():
+                    confidence = float(data[4])
+                    if confidence < 0.6:
+                        continue
+
+                    xmin, ymin, xmax, ymax, label = int(data[0]), int(data[1]), int(data[2]), int(data[3]), int(data[4])
+                    w = xmax - xmin
+                    h = ymax - ymin
+                    object_boxes.append((xmin, ymin, w, h))
+
+                    # YOLO로 감지된 객체에 대해 CSRT 추적기 추가
+                    for (xmin, ymin, w, h) in object_boxes:
+                        # 추적된 객체 부분을 잘라냄
+                        object_region = image_bgr[ymin:ymin+h, xmin:xmin+w]
+                        if object_region.size > 0:
+                            tracker = cv2.TrackerCSRT_create()
+                            tracker.init(image_bgr, (xmin, ymin, w, h))
+                            name="logo"
+                            if(label==0): name="license"                    
+                            trackers.append((tracker, (xmin,ymin,w,h),object_region.size,name))
                 
-                # 이름 표시
-                cv2.putText(image_bgr, label, (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+            # 넓이에 따라 내림차순 정렬
+            trackers.sort(key=lambda x: x[2], reverse=True)  # object region를 기준으로 정렬
+            
+            target_selected = False
 
-                if label == "face":
-                    # 얼굴 부분을 원형으로 블러 처리
-                    if(target_selected==False):
-                        target_selected = True
-                    elif object_region.size > 0:
-                        mask = np.zeros_like(image_bgr)
-                        center = (xmin + w // 2, ymin + h // 2)
-                        radius = int(min(w, h) / 2)
-                        cv2.circle(mask, center, radius + 10, (255, 255, 255), -1)
+            # 추적된 객체들 업데이트
+            for i, (tracker, bbox, object_region, label) in enumerate(trackers): 
+                ret, bbox = tracker.update(image_bgr)
+                if ret:
+                    (xmin, ymin, w, h) = [int(v) for v in bbox]
+                    xmax = xmin + w
+                    ymax = ymin + h
+                    
+                    # 이름 표시
+                    cv2.putText(image_bgr, label, (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
-                        # 블러 처리된 이미지를 생성하고 원형 마스크를 적용하여 블러 처리
-                        blurred_img = cv2.GaussianBlur(image_bgr, (11, 11), 20)
-                        image_bgr = np.where(mask == (255, 255, 255), blurred_img, image_bgr)
-                    else:
-                        # 추적 실패 시 해당 추적기 제거
-                        trackers.pop(i)
-                        tracker_labels.pop(i)
-                else:
-                    if object_region.size > 0:
-                        (xmin, ymin, w, h) = [int(v) for v in bbox]
-                        blurred = cv2.GaussianBlur(object_region, (51, 51), 20)
-                        frame[ymin:ymax, xmin:xmax] = blurred
-                    else:
+                    if label == "face":
+                        # 얼굴 부분을 원형으로 블러 처리
+                        if(target_selected==False):
+                            target_selected = True
+                        elif object_region.size > 0:
+                            mask = np.zeros_like(image_bgr)
+                            center = (xmin + w // 2, ymin + h // 2)
+                            radius = int(min(w, h) / 2)
+                            cv2.circle(mask, center, radius + 10, (255, 255, 255), -1)
+
+                            # 블러 처리된 이미지를 생성하고 원형 마스크를 적용하여 블러 처리
+                            blurred_img = cv2.GaussianBlur(image_bgr, (11, 11), 20)
+                            image_bgr = np.where(mask == (255, 255, 255), blurred_img, image_bgr)
+                        else:
                             # 추적 실패 시 해당 추적기 제거
                             trackers.pop(i)
                             tracker_labels.pop(i)
-        self.is_processing = True
-        print("비디오 변환 처리")
+                    else:
+                        if object_region.size > 0:
+                            # 블러 처리
+                            blurred_img = cv2.GaussianBlur(image_bgr, (51, 51), 20)
+                            # 블러 처리된 이미지에서 해당 영역만 복사
+                            image_bgr[ymin:ymax, xmin:xmax] = blurred_img[ymin:ymax, xmin:xmax]
+                        else:
+                                # 추적 실패 시 해당 추적기 제거
+                                trackers.pop(i)
+                                tracker_labels.pop(i)
+            self.is_processing = True
+            print("비디오 변환 처리")
 
 
-        
-        img = modelutil.process_frame(image_bgr, model2)
-        processing_end_time = time.perf_counter()
-        print(f"모델 처리 시간: {processing_end_time - processing_start_time:.4f} 초")
-        
-        new_frame_start_time = time.perf_counter()
-        new_frame = VideoFrame.from_ndarray(image_bgr, format="bgr24")
-        new_frame.pts = frame.pts
-        print(f"현재 프레임 frame.pts: {frame.pts}")
-        new_frame.time_base = frame.time_base
-        print(f"현재 프레임 time_base: {frame.time_base}")
-        new_frame_end_time = time.perf_counter()
-        print(f"새 프레임 생성 시간: {new_frame_end_time - new_frame_start_time:.4f} 초")
-        print(f"fps: {modelutil.calculate_fps(start_time)}")
-        # 프레임 수 증가
-        frame_count += 1
+            
+            img = modelutil.process_frame(image_bgr, model2)
+            processing_end_time = time.perf_counter()
+            print(f"모델 처리 시간: {processing_end_time - processing_start_time:.4f} 초")
+            
+            new_frame_start_time = time.perf_counter()
+            new_frame = VideoFrame.from_ndarray(image_bgr, format="bgr24")
+            new_frame.pts = frame.pts
+            print(f"현재 프레임 frame.pts: {frame.pts}")
+            new_frame.time_base = frame.time_base
+            print(f"현재 프레임 time_base: {frame.time_base}")
+            new_frame_end_time = time.perf_counter()
+            print(f"새 프레임 생성 시간: {new_frame_end_time - new_frame_start_time:.4f} 초")
+            print(f"fps: {modelutil.calculate_fps(start_time)}")
+            # 프레임 수 증가
+            frame_count += 1
 
-        # FPS 계산
-        if time.perf_counter() - fps_start_time >= 1.0:  # 1초마다 FPS 계산
-            fps = frame_count / (time.perf_counter() - fps_start_time)
-            print(f"현재 FPS: {fps:.2f}")
-            frame_count = 0  # 카운터 초기화
-            fps_start_time = time.perf_counter()  # 시간 초기화
+            # FPS 계산
+            if time.perf_counter() - fps_start_time >= 1.0:  # 1초마다 FPS 계산
+                fps = frame_count / (time.perf_counter() - fps_start_time)
+                print(f"현재 FPS: {fps:.2f}")
+                frame_count = 0  # 카운터 초기화
+                fps_start_time = time.perf_counter()  # 시간 초기화
 
-        self.is_processing = False
-        total_time = time.perf_counter() - start_time
-        print(f"총 처리 시간: {total_time:.4f} 초")
-        return new_frame
+            self.is_processing = False
+            total_time = time.perf_counter() - start_time
+            print(f"총 처리 시간: {total_time:.4f} 초")
+            return new_frame
+        elif self.kind == "audio":
+            #print("오디오 처리 중")
+            audio_data = frame.to_ndarray()
+            audio_buffer.append(audio_data)
+            return frame
 
 @router.get("/", response_class=HTMLResponse)
 async def index():
