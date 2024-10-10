302bb5adc6c6409b369908b5a7a56f27e093f40c
sangmu <dltkdandl@naver.com>
2024-10-10 16:18:30 +0900

    feat: 첫 페이지 구현..

--- fe/lib/view/pages/landing_page.dart
+++ fe/lib/view/pages/landing_page.dart
@@ -1,9 +1,10 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_web_auth/flutter_web_auth.dart';
 import 'package:uni_links/uni_links.dart';
-import 'dart:async';
+import 'package:video_player/video_player.dart';
 
 class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
@@ -13,36 +14,38 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
-  final storage = const FlutterSecureStorage(); // Secure Storage 인스턴스 생성
+  final storage = const FlutterSecureStorage();
   StreamSubscription? _linkSubscription;
+  VideoPlayerController? _videoController;
 
   @override
   void initState() {
     super.initState();
     _initUniLinks();
+    _initializeVideo();
+  }
+
+  Future<void> _initializeVideo() async {
+    _videoController = VideoPlayerController.asset('assets/blurvideo.mp4')
+      ..initialize().then((_) {
+        setState(() {
+          _videoController?.setLooping(true);
+          _videoController?.play();
+        });
+      });
   }
 
   Future<void> _initUniLinks() async {
-    // 초기 링크 설정
     _linkSubscription = linkStream.listen((String? link) {
       if (link != null) {
-        // 콜백 URL에서 accessToken과 refreshToken 추출
         final uri = Uri.parse(link);
         final accessToken = uri.queryParameters['accessToken'];
         final refreshToken = uri.queryParameters['refreshToken'];
 
-        print('accessToken: $accessToken');
-        print('refreshToken: $refreshToken');
-        print('uri: $uri');
         if (accessToken != null && refreshToken != null) {
-          // Secure Storage에 토큰 저장
           storage.write(key: 'accessToken', value: accessToken);
           storage.write(key: 'refreshToken', value: refreshToken);
-
-          print('Tokens saved in secure storage');
           Get.toNamed('/face_registration');
-        } else {
-          print('Token not found in the callback URL');
         }
       }
     });
@@ -51,6 +54,7 @@ class _LandingPageState extends State<LandingPage> {
   @override
   void dispose() {
     _linkSubscription?.cancel();
+    _videoController?.dispose();
     super.dispose();
   }
 
@@ -58,84 +62,76 @@ class _LandingPageState extends State<LandingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage('assets/wallpaper.jpg'),
-          ),
-        ),
-        child: Stack(
-          children: [
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(height: 150),
-                    Text(
-                      'BBIP',
-                      style: TextStyle(
-                        fontSize: 48,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 60),
-                child: ElevatedButton.icon(
-                  onPressed: () {
-                    _signInWithGoogle(context); // Google 로그인 함수 호출
-                  },
-                  icon: Image.asset(
-                    'assets/google_logo.png',
-                    height: 24.0,
-                    width: 24.0,
-                  ),
-                  label: const Text(
-                    'Sign In with Google',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.black,
+      body: Stack(
+        children: [
+          _videoController != null && _videoController!.value.isInitialized
+              ? SizedBox.expand(
+                  child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: SizedBox(
+                      width: _videoController!.value.size.width,
+                      height: _videoController!.value.size.height,
+                      child: VideoPlayer(_videoController!),
                     ),
                   ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 12, vertical: 12),
-                  ),
+                )
+              : const Center(child: CircularProgressIndicator()),
+          const Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: EdgeInsets.only(top: 150),
+              child: Text(
+                'BBIP',
+                style: TextStyle(
+                  fontSize: 48,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 60),
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  _signInWithGoogle(context);
+                },
+                icon: Image.asset(
+                  'assets/google_logo.png',
+                  height: 24.0,
+                  width: 24.0,
+                ),
+                label: const Text(
+                  'Sign In with Google',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 
   Future<void> _signInWithGoogle(BuildContext context) async {
-    print('signInWithGoogle Pressed');
-
     try {
-      // Web Auth 플로우를 사용하여 Google 로그인 페이지로 리디렉션
       await FlutterWebAuth.authenticate(
         url: 'http://j11a203.p.ssafy.io:8080/oauth2/authorization/google',
-        callbackUrlScheme: 'bbip', // 이 스키마는 콜백 URL을 설정할 때 필요
+        callbackUrlScheme: 'bbip',
       );
-
-      // 성공적으로 로그인 후, uni_links가 처리할 수 있도록 아무 것도 하지 않음
-      print('Redirecting to uni_links');
     } catch (e) {
       print('Failed to authenticate with Google: $e');
     }
--- fe/lib/view/pages/splash_screen.dart
+++ fe/lib/view/pages/splash_screen.dart
@@ -1,4 +1,4 @@
-// splash_screen.dart
+import 'dart:ui'; // BackdropFilter를 사용하기 위해 추가
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,53 +11,100 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _fadeAnimation;
+  late AnimationController _blurController;
+  late Animation<double> _blurAnimation;
 
   @override
   void initState() {
     super.initState();
 
-    // 3초 동안 애니메이션 진행
-    _controller = AnimationController(
+    // 블러 애니메이션 컨트롤러 초기화
+    _blurController = AnimationController(
       duration: const Duration(seconds: 3),
       vsync: this,
     );
 
-    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+    _blurAnimation = Tween<double>(begin: 0.0, end: 6.0).animate(
+      CurvedAnimation(
+        parent: _blurController,
+        curve: Curves.easeOut,
+      ),
+    );
 
-    // 애니메이션 시작
-    _controller.forward();
+    // 1초 후에 애니메이션 시작
+    Future.delayed(const Duration(seconds: 1), () {
+      _blurController.forward();
+    });
 
-    // 3초 후에 자동으로 페이지 이동
-    Future.delayed(const Duration(seconds: 5), () {
-      Get.offNamed('/landing'); // 3초 후에 LandingPage로 이동
+    // 3초 후에 landing 페이지로 이동
+    Future.delayed(const Duration(seconds: 4), () {
+      Get.offNamed('/landing');
     });
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _blurController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlue, // 배경색 설정
-      body: Center(
-        child: FadeTransition(
-          opacity: _fadeAnimation,
-          child: const Text(
-            'BBIP',
-            style: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.bold,
-              color: Colors.white, // 텍스트 색상
+      backgroundColor: const Color(0xff008fff),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Center(
+            child: Image.asset(
+              'assets/splashscreen.png',
+              width: 300, // 이미지 너비 조정
+              height: 300, // 이미지 높이 조정
             ),
           ),
-        ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 270.0), // 필요에 따라 조정
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _buildIcon('assets/brand.png'),
+                  const SizedBox(width: 25), // 아이콘 간격
+                  _buildIcon('assets/face-detection.png'),
+                  const SizedBox(width: 25), // 아이콘 간격
+                  _buildIcon('assets/license-plate.png'),
+                ],
+              ),
+            ),
+          ),
+          // 전체 화면 블러 효과
+          Positioned.fill(
+            child: AnimatedBuilder(
+              animation: _blurAnimation,
+              builder: (context, child) {
+                return BackdropFilter(
+                  filter: ImageFilter.blur(
+                    sigmaX: _blurAnimation.value,
+                    sigmaY: _blurAnimation.value,
+                  ),
+                  child: Container(
+                    color: Colors.transparent, // 배경색을 투명으로 설정
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget _buildIcon(String assetPath) {
+    return Image.asset(
+      assetPath,
+      width: 50, // 아이콘 너비 조정
+      height: 50, // 아이콘 높이 조정
     );
   }
 }
